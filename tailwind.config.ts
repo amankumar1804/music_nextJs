6c959ee668fcc6ba00553aef34c34bc55a52f995
Aman Kumar <107541415+amankumar1804@users.noreply.github.com>
2024-04-23 01:46:10 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,10 +27,10 @@ const config: Config = {
           },
         },
         spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
+  "0%": {
+    opacity: "0",
+    transform: "translate(-72%, -62%) scale(0.5)"
+  },
           "100%": {
             opacity: 1,
             transform: "translate(-50%,-40%) scale(1)",
